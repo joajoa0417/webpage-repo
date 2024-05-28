b90f5b8fd0869213471418ad742c1d4a49f20e7b
KimHagyung <joajoa171717@gmail.com>
2024-05-28 13:05:41 +0900

    NavigationBar 생성

---
+++ src/header/NavigationBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import { Nav, Navbar, Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.css';
+
+function NavigationBar() {
+  return (
+      <div className="NavigationBar">
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Calender">Calender</Nav.Link>
+            <Nav.Link href="#Memo">Memo</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="검색어를 입력하세요"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+          <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavigationBar;
